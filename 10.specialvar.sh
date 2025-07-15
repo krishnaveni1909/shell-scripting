4d66a66e0000294d5f9b1344e2850900fd1131e0
KRISHNA <krishnabackup19@gmail.com>
2025-07-15 19:24:40 +0530

    Special variables details are added

---
+++ 10.specialvar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed                    : $@"
+echo "Number of Variables                     : $#"
+echo "Script name                             : $0"
+echo "Parent working directory                : $PWD"
+echo "Home directory of current user          : $HOME"
+echo "Which user is running this script       : $USER"
+echo "Process id of current script            : $$"
+sleep 60&
+echo "Process id of last command in background: $!"
